44f9c7f9c753cbaacae431cbce30176e55c7a902
wangsh <wugcelama19@gmail.com>
2021-06-18 17:03:32 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//Object create 
+const Inventary_Data = {
+
+    mask:900,
+    sanitizer:500,
+    paracetamol:900
+}
+
+//Object create 
+
+const Price_List = {
+
+    mask:20,
+    sanitizer:100,
+    paracetamol:50
+}
+
+//Object call
+console.log(Inventary_Data);
+
+//Object call
+console.log(Price_List);
